821b104550340d4cc44675520ed876cd3f2cb0f4
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-04-03 12:02:18 -0700

    User can input only positive numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,28 @@
-// 
+// Business logic
 
 
 function numberReplace(userNumber) {
   var numArray = [];
+  if (userNumber < 0) {
+    return ("Please enter the positive number");
+  } else { 
+
   
-  for (var i=0; i <= userNumber; i++) {
-    var addString = i.toString();
-    if (addString.includes("3")) {
-      numArray.push("Won't you be my neighbor?");
-    } else if (addString.includes("2")) {
-      numArray.push("Boop!");
-    } else if (addString.includes("1")) {
-      numArray.push("Beep!");
-    } else {
-    numArray.push(i);
-    console.log(numArray);
-    }
-  };
-  return numArray;
+    for (var i=0; i <= userNumber; i++) {
+      var addString = i.toString();
+      if (addString.includes("3")) {
+        numArray.push("Won't you be my neighbor?");
+      } else if (addString.includes("2")) {
+        numArray.push("Boop!");
+      } else if (addString.includes("1")) {
+        numArray.push("Beep!");
+      } else {
+      numArray.push(i);
+      console.log(numArray);
+      }
+    };
+    return numArray;
+  }
 
   
 
