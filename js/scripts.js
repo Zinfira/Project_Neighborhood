35ef3056d421c056a4f296bcaebe99e5063021b6
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-04-03 13:32:02 -0700

    Add background image

--- js/scripts.js
+++ js/scripts.js
@@ -7,20 +7,17 @@ function numberReplace(userNumber) {
     return ("Please enter the positive number");
   } else { 
 
-  var text = "";
-  var i;
-    for (var i=0; i <= userNumber; i++) {
+    for (var i=0;  i <= userNumber;  i++) {
       var addString = i.toString();
-      text += " " + i + "<br>";
-
+      
       if (addString.includes("3")) {
-        numArray.push("Won't you be my neighbor?");
+        numArray.push(" Won't you be my neighbor?");
       } else if (addString.includes("2")) {
-        numArray.push("Boop!");
+        numArray.push(" Boop!");
       } else if (addString.includes("1")) {
-        numArray.push("Beep!");
+        numArray.push(" Beep!");
       } else {
-      numArray.push(i);
+      numArray.push(" "+i);
       console.log(numArray);
       }
     };
