8900d6ad44214f53d7d78653e29a8f52b0329dfa
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-04-03 09:55:23 -0700

    Edit html and js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 // 
-
+function range(start, end) {
+  return Array(end - start + 1).fill().map((_, idx) => start + idx)
+}
+var result = range(9, 18); // [9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+console.log(result);
 
 
 
@@ -8,7 +12,9 @@ $(document).ready(function() {
   $("form#form1").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("input#numInput").val());
-    
-  
+    var result = range(start,end);
+    if (result) {
+      $(".result").text(result);
+    }
   });
 });
