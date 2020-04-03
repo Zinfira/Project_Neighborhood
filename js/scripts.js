3a7d617ed166178e0aebb31a7c44ddbc00f7dce0
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-04-03 08:48:54 -0700

    Add basic html elements and start to make user interface logic

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// 
+
+
+
+
+//UI logic
+$(document).ready(function() {
+  $("form#form1").submit(function(event) {
+    event.preventDefault();
+    var number = parseInt($("input#numInput").val());
+    
+  
+  });
+});
